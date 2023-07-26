1e46b8f7ab096ab86eda3e47334bb2798d6985a2
dcm <dcm@emwun.local>
2023-07-26 18:21:28 +0930

    implemented new demo and font_print function

--- main.c
+++ main.c
@@ -1,160 +1,45 @@
 ﻿#include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <stdio.h>
 
+#include "stdbool.h"
+#include "pico/stdlib.h"
 #include "pico/time.h"
 #include "hardware/timer.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
 
-#include "types.h"
-#include "lcd.h"
 #include "surface.h"
+#include "lcd.h"
+#include "types.h"
 #include "sprite.h"
+#include "font.h"
 
 
-//plz no lawyers its just a joke
-char LogoChars[] = { //52x8
-    "##   ## ##                             ##           " \
-    "###  ## ##        ##                   ##        ## " \
-    "###  ##          ####                  ##       #  #" \
-    "## # ## ## ## ##  ##  ####  ## ##   #####  ####  ## " \
-    "## # ## ## ### ## ## ##  ## ### ## ##  ## ##  ##    " \
-    "##  ### ## ##  ## ## ###### ##  ## ##  ## ##  ##    " \
-    "##  ### ## ##  ## ## ##     ##  ## ##  ## ##  ##    " \
-    "##   ## ## ##  ## ##  ##### ##  ##  #####  ####     "
-};
 
+int main () {
+    stdio_init_all();
 
-//struct to pass between update() and render() functions
-typedef struct {
-    uint64_t frames;
-    Surface *screen, *logo;
-    Rect screenRect, logoRect;
-    uint8_t logoModeX, logoModeY;
-    uint8_t r,g,b;
-    Sprite *sprite;
-} State;
+    adc_init();
+    adc_set_temp_sensor_enabled(true);
+    adc_gpio_init(PICO_VSYS_PIN);
+    adc_select_input(4); //temp
 
-
-void update (float delta, State *state) {
-    sprite_update(state->sprite);
-
-    state->r--;// = rand() % 256;
-    state->g++;// = rand() % 256;
-    state->b--;// = rand() % 256;
-
-    if (state->logoModeX == 1) {
-        state->screenRect.x += 2;
-        if (--state->screenRect.w < 15) state->screenRect.w = 15;
-        if (state->screenRect.x > LCD_WIDTH - state->screenRect.w) state->logoModeX = -1;
-    } else {
-        state->screenRect.x -= 2;
-        if (++state->screenRect.w > 100) state->screenRect.w = 100;
-        if (state->screenRect.x < 0) state->logoModeX = 1;
-    }
-
-    if (state->logoModeY == 1) {
-        state->screenRect.y += 2;
-        if (--state->screenRect.h < 6) state->screenRect.h = 6;
-        if (state->screenRect.y > LCD_HEIGHT - state->screenRect.h) state->logoModeY = -1;
-    } else {
-        state->screenRect.y -= 2;
-        if (++state->screenRect.h > 100) state->screenRect.h = 100;
-        if (state->screenRect.y < 0) state->logoModeY = 1;
-    }
-}
-
-
-void render (float delta, State *state) {
-    //primes to use with lcd_draw_surface_checkered()
-    uint32_t primes[] = { 
-           223, 738797, 255253, 947357, 760231, 558611, 349211, 185593, 895171, 532163,
-        825779, 199819, 493523, 101383, 918949, 254827, 340577, 893603, 707249, 346369,
-        276173,  71761,  92639,  86293,  22343,  50111,  76673,  47351,  20981,  32633 
-    };
-
-    //Clear screen to current r,g,b value
-    surface_fill_rgb(state->screen, state->r, state->g, state->b);
-
-    //Alternatively, you can surface_fill() with a 16-bit colour value
-    //surface_fill(state->screen, WHITE);
-
-    //Copy the 'logo' surface to the 'screen', using the logoRect to specify the region of the logo
-    //to be copied, while the screenRect specifies the destination position (no scaling is done)
-    //The mask colour will be ignored in the 'logo' surface, allowing for transparent backgrounds
-    //surface_blit_mask(state->screen, state->logo, &state->screenRect, &state->logoRect, WHITE);
-
-    //Or do the same but with scaling...
-    surface_scaleblit_mask(state->screen, state->logo, &state->screenRect, &state->logoRect, WHITE);
-
-    Rect rect;
-    rect.x = rect.y = 0;
-    rect.w = 160;
-    rect.h = 40;
-    sprite_draw_mask(state->screen, state->sprite, &rect, WHITE);
-
-    //Send the 'screen' surface to the LCD panel (sequential pixels)
-    //lcd_draw_surface(state->screen);
-
-    //Send the 'screen' surface to the LCD panel in subdivided tiles
-    lcd_draw_surface_checkered(state->screen, 64 + (rand() % 2 == 0 ? 64 : 0), primes[state->frames % 30]);
-
-    state->frames++;
-}
-
-
-int main(void)
-{
-    State *state = (State *)malloc(sizeof(State));
-
-    //configure the LCD for use
     lcd_init();
-    lcd_set_backlight(100); 
+    lcd_set_backlight(50);
+    Surface *screen = surface_create(LCD_WIDTH, LCD_HEIGHT);
 
-    //create the surface to use as the screen buffer
-    state->screen = surface_create(LCD_WIDTH, LCD_HEIGHT);
-    surface_fill(state->screen, WHITE);
+    char str[256];
 
-    state->logo = surface_create(52, 8);
-    surface_load(state->logo, LogoChars, 52 * 8, RED, WHITE);
-
-    state->r = 255;
-    state->g = 63;
-    state->b = 100;
-
-    state->logoRect.x = 0;
-    state->logoRect.y = 0;
-    state->logoRect.w = 52;
-    state->logoRect.h = 8;
-    state->screenRect.x = rand() % (LCD_WIDTH - 104);
-    state->screenRect.y = rand() % (LCD_HEIGHT - 16);
-    state->screenRect.w = 104;
-    state->screenRect.h = 16;
-    state->logoModeX = -1 + rand() % 3;
-    state->logoModeY = -1 + rand() % 3;
-
-
-    state->sprite = sprite_create(state->logo, 13, 8, 0, 3, 1.0f);
-
-    state->frames = 0;
-
-    uint64_t tLastUpdate = time_us_64() + 1, tLastRender = time_us_64() + 1;
-
-    while (true) {
-        uint64_t t = time_us_64();
-        float deltaUpdate = (t - tLastUpdate) / 1000000.0f;
-        float deltaRender = (t - tLastRender) / 1000000.0f;
-
-        if (deltaUpdate >= 0.01f) {
-            tLastUpdate = t;
-            update(deltaUpdate, state);
-        }
-
-        if (deltaRender >= 0.03333f) {
-            tLastRender = t;
-            render(deltaRender, state);
-        }
+    while(1) {
+        surface_fill(screen, BLACK);
+        float adc_result = (float)adc_read() * (3.3f / (1 << 12));
+        float tempC = 27.0f - (adc_result - 0.706f) / 0.001721f;
+        sprintf(str, "Temp: %.2fc", tempC);
+        font_print(screen, str, 1, 1, GREEN);
+        lcd_draw_surface(screen);
+        sleep_ms(500);
     }
-
     return 0;
 }
-
